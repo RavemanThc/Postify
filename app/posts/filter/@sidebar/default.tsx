01b1bca43a45ddba59d56cdf209a04c783f3fad3
Ubuntu <vlad.garkusha1999@gmail.com~>
2026-04-26 14:34:41 +0300

    chanfe styles

--- app/posts/filter/@sidebar/default.tsx
+++ app/posts/filter/@sidebar/default.tsx
@@ -1,3 +1,17 @@
 export default function SideBar() {
-  return <div style={{ color: 'white', backgroundColor: 'green' }}>SideBar</div>;
+  return (
+    <div
+      style={{
+        color: "white",
+        backgroundColor: "rgba(0, 128, 0, 0.7)",
+        fontSize: "20px",
+        backdropFilter: "blur(10px)",
+        WebkitBackdropFilter: "blur(10px)",
+        padding: "15px",
+        borderRadius: "5px",
+      }}
+    >
+      SideBar
+    </div>
+  );
 }
